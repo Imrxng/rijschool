a3dff0a8d0457c4cffb65546f43ae209d683e0ae
Imran <imgha@hotmail.com>
2024-12-23 19:41:45 +0100

    m

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,14 +1,12 @@
 import Header from "@/components/header";
-import "@/styles/header.component.css";
-import dynamic from "next/dynamic";
+import Map from "@/components/Map";
 
-const DynamicMap = dynamic(() => import("@/components/Map"), { ssr: false });
 
 const Contact = () => {
     return (
         <div>
             <Header/>
-            <DynamicMap/>
+            <Map/>
         </div>
     );
 }
